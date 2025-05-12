f1dfaf0f8e3f7c354af70f5748381a30daeb3b6c
NguyenHuyHoang20225845 <hoang.nh225845@sis.hust.edu.vn>
2025-05-12 22:35:12 +0700

    More traps

--- Assets/Scripts/Enemies/EnemyDamage.cs
+++ Assets/Scripts/Enemies/EnemyDamage.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyDamage : MonoBehaviour
 {
-    [SerializeField] private float damage;
+    [SerializeField] protected float damage;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
             collision.GetComponent<Health>().TakeDamage(damage);
